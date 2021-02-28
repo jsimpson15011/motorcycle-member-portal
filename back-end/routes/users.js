da31de48cdb9eb13cc11813e75d59dd9ef245875
jsimpson15011 <joseph.franklin.simpson@gmail.com>
2021-02-28 10:20:57 -0700

    Clean up user routes on backend

--- back-end/routes/users.js
+++ back-end/routes/users.js
@@ -4,8 +4,8 @@ const bcrypt = require('bcrypt')
 const saltRounds = 10
 
 let fakeUsers = [
-  {id: 2, userName: "normal", email:"test@email.com",isAdmin: false, hash: "fakehash"},
-  {id: 3, userName: "admin", isAdmin: true, email:"test2@email.com", hash: "fakehash"},
+  {id: 2, userName: "normal", email: "test@email.com", isAdmin: false, hash: "fakehash"},
+  {id: 3, userName: "admin", isAdmin: true, email: "test2@email.com", hash: "fakehash"},
 ]
 
 /* GET users listing. */
@@ -19,26 +19,28 @@ router.get('/:id', function (req, res, next) {
 
   const pass = "pass"
 
-  const user = fakeUsers.filter(user=> {
+  const user = fakeUsers.filter(user => {
     return user.id === id
   })[0]
 
-  bcrypt.genSalt(saltRounds, function (err, salt) {
-    bcrypt.hash(pass, salt, function (err, hash){
-      res.send(user ? user : "User Not Found")
-    })
-  })
+  res.send(user ? user : "User Not Found")
 
 })
 
 /*POST new user*/
-router.post('/', function (req, res, next){
+router.post('/', function (req, res, next) {
   const newUser = req.body
-  const nextId = fakeUsers[fakeUsers.length-1].id + 1 // When we set up a database we'll just use the autoincrement
+  const nextId = fakeUsers[fakeUsers.length - 1].id + 1 // When we set up a database we'll just use the autoincrement
 
   bcrypt.genSalt(saltRounds, function (err, salt) {
-    bcrypt.hash(newUser.password, salt, function (err, hash){
-      fakeUsers.push({id: nextId, userName: newUser.userName, isAdmin: newUser.isAdmin, email: newUser.email, hash: hash})
+    bcrypt.hash(newUser.password, salt, function (err, hash) {
+      fakeUsers.push({
+        id: nextId,
+        userName: newUser.userName,
+        isAdmin: newUser.isAdmin,
+        email: newUser.email,
+        hash: hash
+      })
       res.send().status(200)
     })
   })
